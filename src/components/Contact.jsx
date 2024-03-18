f92d8b47acd0428e85fa15d14f2f96f3fcd836d4
Sivasankaran <65439996+siva12143@users.noreply.github.com>
2024-03-18 16:33:53 +0530

    Add files via upload

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,20 +2,9 @@ import React from 'react'
 import emailjs from "@emailjs/browser";
 import contact from '../images/contact.jpg'
 import { Link } from 'react-router-dom'
-import { form } from "react";
 
 export default function Contact() {
-    const sendEmail = (e) => {
-        e.preventDefault();
 
-        emailjs.sendForm('service_84jokq2', 'template_za0rgsf', form.current, 'qa9tbPiqQ7sLhOrRU')
-            .then((result) => {
-                console.log(result.text);
-            }, (error) => {
-                console.log(error.text);
-            });
-        e.target.reset()
-    };
     return (
         <div>
             <footer className="contacts">
@@ -36,15 +25,6 @@ export default function Contact() {
                                 <span>Tirunelveli, TamilNadu - 627426</span>
                             </div>
                         </div>
-                        {/* <form ref={form} onSubmit={sendEmail} className="contact2">
-                        <div>
-                            <input type="text" placeholder="User Name"/>
-                            <input type="email" placeholder="Email"/>
-                            <input type="text" placeholder="Number"/>
-                        </div>
-                        <textarea name="" id="" cols="30" rows="10" placeholder="Message..."></textarea>
-                        <a ><input type="submit" className='submit_btn' value="Submit"/></a>
-                    </form> */}
                     </div>
                 </div>
             </footer>
