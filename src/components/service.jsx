d5efcd6d5f8e0889444aac496889a23f553212fc
Sivasankaran <65439996+siva12143@users.noreply.github.com>
2024-09-02 20:09:27 +0530

    Update service.jsx

--- src/components/service.jsx
+++ src/components/service.jsx
@@ -4,37 +4,37 @@ export default function Experience() {
     return (
         <div>
             <div className="service" id="service">
-                <div className="service_container">
-                    <h1 className="cont_title service_title">Experience</h1>
-                    <div className="service_inner">
-                        <div className="service1">
-                            <div>
-                                <img className="service1img" src="images/service_icon2.png" alt="" />
-                                <h2>Prognamik Technologies</h2>
-                                <h3>Frontend Developer</h3>
-                                <p>Over One years at Prognamik Technologies, I've spearheaded the development of six dynamic projects utilizing ReactJS, blending innovation with seamless functionality for optimal user experiences.</p>
-                            </div>
-                        </div>
-                        <div className="service2">
-                            <div>
-                                <img src="images/service_icon4.png" alt="" />
-                                <h2>Nurture Spark Digital</h2>
-                                <h3>Frontend Trainer</h3>
-                                <p>During a six-month tenure at Nurture Spark Digital, I led the creation of two cutting-edge projects leveraging ReactJS, driving forward digital innovation with streamlined solutions and immersive user interfaces.</p>
-                            </div>
-
-                        </div>
-                        <div className="service3">
-                            <div>
-                                <img src="images/service_icon3.png" alt="" />
-                                <h2>Zitraar</h2>
-                                <h3>Zoho Developer</h3>
-                                <p>Developed and customized Zoho Creator solutions, integrated third-party applications, and automated workflows to enhance operational efficiency and data accuracy.</p>
-                            </div>
-                        </div>
-                    </div>
+    <div className="service_container">
+        <h1 className="cont_title service_title">Experience</h1>
+        <div className="service_inner">
+            <div className="service1">
+                <div>
+                    <img className="service1img" src="images/service_icon2.png" alt="" />
+                    <h2>Prognamik Technologies</h2>
+                    <h3>Frontend Developer</h3>
+                    <p>Over Six Month at Prognamik Technologies, I've spearheaded the development of six dynamic projects utilizing ReactJS, blending innovation with seamless functionality for optimal user experiences.</p>
                 </div>
             </div>
+            <div className="service2">
+                <div>
+                    <img src="images/service_icon4.png" alt="" />
+                    <h2>Npng Tech Works</h2>
+                    <h3>Frontend Developer</h3>
+                    <p>During One Year tenure at Npng Tech Works, I led the creation of two cutting-edge projects leveraging ReactJS, driving forward digital innovation with streamlined solutions and immersive user interfaces.</p>
+                </div>
+
+            </div>
+            <div className="service3">
+                <div>
+                    <img src="images/service_icon3.png" alt="" />
+                    <h2>Npng</h2>
+                    <h3>Frontend Trainer</h3>
+                    <p>During Six Month of experience as a frontend trainer at NPNG, where I taught HTML, CSS, JavaScript, and contemporary frontend technologies</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
         </div>
     )
 }
